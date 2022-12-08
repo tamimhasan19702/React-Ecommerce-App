62b40d144eac3f6115aed21631e393beef12f56e
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-09 01:39:32 +0600

    Applying the changes in app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(){
+    super()
+    this.state = {
+      meaningOflLife : 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState({meaningOflLife: this.state.meaningOflLife + 1});
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOflLife}
+          </p>
+          <button onClick={this.handleClick}>Update State</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
