e521e42947fa8d9310d74cb59a24bbd3a76c9b4c
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-10 18:51:45 +0600

    setting up for e-commerce app

--- src/App.js
+++ src/App.js
@@ -2,63 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+function App () {
   
-  constructor(Props){
-    super(Props)
-    this.state = {
-      showChild: true,
-      text: ''
-    }
-  }
-
-  // handleClick = () => {
-  //   this.setState((prevState , prevProps) => {
-     
-  //     return {meaningOflLife: prevState.
-  //       meaningOflLife + prevProps.increment}
-    
-  //   }, 
-  //   () => console.log(this.state.meaningOflLife));
-
-  // }
-
-
-  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.meaningOflLife}
-          </p>
-          
-          <button 
-          onClick={() => 
-            this.setState( state => ({
-              showChild: !state.showChild
-            }))
-          }
-            >
-            Toggle LifeCycle
-            </button>
-            
-          <button
-          onClick={() => 
-          this.setState( state => ({
-            text: state.text + '_hello'
-          }))
-          }
-          >update Text
-          </button>
-
-
-         {/* {this.state.showChild ? } */}
-
-        </header>
-      </div>
+      <div>Hello World</div>
     );
-  }
 }
 
 export default App;
