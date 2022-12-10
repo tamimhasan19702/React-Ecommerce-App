dd13cb732c793e0bbb2f36e852a33b72a209993c
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-10 21:24:03 +0600

    directory object created

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,42 @@
+import React ,{Component} from 'react'
+
+class directory extends Component {
+constructor(){
+    super();
+
+  this.state = {
+    section: [
+    {
+        title:'hats',
+        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        id: 1
+    },
+    {
+        title:'jackets',
+        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        id: 2
+    },
+    {
+        title:'Sneakers',
+        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        id: 3
+    },
+    {
+        title:'womans',
+        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        id: 1
+    },
+    {
+        title:'mans',
+        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        id: 1
+    },
+]
+  }
+}
+
+
+
+}
+
+export default directory
