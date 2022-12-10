b16cb2b24847619ec7840dc4927a97cd7fddad3a
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-10 21:15:28 +0600

    components updates

--- src/pages/homepage/hompage.component.jsx
+++ src/pages/homepage/hompage.component.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './homepage.scss';
+import MenuItem from '../../components/menu-item/menu-item.component';
 
 function Homepage() {
   return (
     <div className='homepage'>
       <div className="directory-menu">
-        <div className="menu-item">
-            <div className="content">
-                <h1 className="title">Hats</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
+        
+        <MenuItem title="Hats"/>
         <div className="menu-item">
             <div className="content">
                 <h1 className="title">JACKETS</h1>
