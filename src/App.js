f79482aad702b3abf909408bfaae6c870ffadf44
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-10 18:58:57 +0600

    homepage component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './hompage.component';
 
 function App () {
   
     return (
-      <div>Hello World</div>
+      <div>
+        <Homepage />
+      </div>
     );
 }
 
