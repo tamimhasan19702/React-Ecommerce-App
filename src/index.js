58a9122fa9530faba2b28160db1a2192efa2ce01
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-12 02:32:36 +0600

    react router needs more practice

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App increment={1}/>
+    <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
 
 
