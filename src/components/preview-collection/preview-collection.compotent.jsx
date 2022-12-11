2e14deb0ff95c0300e7f531216698d8ea7168dd1
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-11 22:47:54 +0600

    collection preview component created

---
+++ src/components/preview-collection/preview-collection.compotent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './preview-collection.style.css';
+
+function previewCollection({title, items}) {
+  return (
+    <div className='collection-preview'>
+     <h1 className='title'>{title.toUpperCase()}</h1>
+     <div className="preview">
+        {
+              items.map(item => (
+                <div key={item.id}>{item.name}</div>
+            ))
+        }
+     </div>
+    </div>
+  )
+}
+
+export default previewCollection
