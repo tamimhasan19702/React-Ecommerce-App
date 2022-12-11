99f124ee2e0db0cbf78b75107f0d17d61cf7fe0b
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-11 20:13:14 +0600

    added tricky destructuring to the items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ constructor(){
     {
         title:'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1
+        id: 1,
+        linkUrl: 'hats',
     },
     {
         title:'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2
+        id: 2,
+        linkUrl: '',
     },
     {
         title:'Sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3
+        id: 3,
+        linkUrl: '',
     },
     {
         title:'womans',
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
-        id: 1
+        id: 4,
+        linkUrl: '',
     },
     {
         title:'mans',
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
-        id: 1
+        id: 5,
+        linkUrl: '',
     },
 ]
   }
@@ -43,13 +48,10 @@ render(){
     return(
         <div className="directory-menu">
         {
-            this.state.section.map(({title,imageUrl, id, size}) => (
+            this.state.section.map(({id, ...otherSectionProps}) => (
                 <MenuItem 
                 key={id}
-                title={title}
-                imageUrl={imageUrl}
-                size={size}
-                history={this.props.history}
+                {...otherSectionProps}
                 />
             ))
         }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './menu-item.scss';
 
-function MenuItem({title, imageUrl, size}) {
+
+function MenuItem({title, imageUrl, size, history }) {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push()}>
           
           <div 
           className="background-image" 
