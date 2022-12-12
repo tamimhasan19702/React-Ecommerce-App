9340e34bd10ae88f164cea8911b697ceb5ef6b63
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-12 13:37:28 +0600

    react- router finally worked

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
 import React from 'react';
 import './App.css';
-import { Switch ,Route, Link } from 'react-router-dom';
-// import Homepage from './pages/homepage/hompage.component';
-// import shopComponent from './pages/shop/shop.component';
-// import HeaderComponent from './components/header/Header.component';
+import { Switch ,Route } from 'react-router-dom';
+import Homepage from './pages/homepage/hompage.component';
+import shopComponent from './pages/shop/shop.component';
+import HeaderComponent from './components/header/Header.component';
 
-
-const Test1 = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h1>Homepage</h1>
-            <Link to={`${props.match.url}/14`}>test2</Link>
-        </div>
-    )
-}
-
-const Test2 = () => {
-    return(<div>
-        <h1>test 2</h1>
-    </div>)
-}
-const Test3 = (props) => {
-    console.log(props)
-    return(<div>
-        <h1>test 3 {props.match.params.testId}</h1>
-    </div>)
-}
  
 function App () {
   
     return (
       <div>
-        {/* <HeaderComponent /> */}
+        <HeaderComponent />
 
         <Switch >
           
-            {/* <Route exact path='/'  component={Homepage} /> 
+            <Route exact path='/'  component={Homepage} /> 
 
-            <Route path='/shop' component={shopComponent}/>  */}
+            <Route path='/shop' component={shopComponent}/> 
 
-            <Route  path='/' component={Test1} />
-            <Route  path='/test2' component={Test2} />
-            <Route exact path='/test3/:testId' component={Test3} />
             
 
         </Switch>
