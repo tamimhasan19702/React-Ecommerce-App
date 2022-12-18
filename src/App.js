132ae7aec1cfe6fcfde5f9d6254bc8e60309a8db
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-18 18:44:50 +0600

    firebase firestore library to store data in current user info

--- src/App.js
+++ src/App.js
@@ -20,10 +20,25 @@ class App extends Component {
     unsubscribeFromAuth = null;
 
     componentDidMount(){
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-            this.setState({ currentUser: user })
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if(userAuth){
+            const userRef = await createUserProfileDocument(userAuth);
 
-            
+        userRef.onSnapshot(snapshot => {
+           this.setState({
+            currentUser: {
+                id: snapshot.id,
+                ...snapshot.data()
+                }
+              }, () => {
+                console.log(this.state)
+            })
+           });
+           
+         } 
+           this.setState({
+            currentUser: userAuth
+           }) 
         })
     }
   
