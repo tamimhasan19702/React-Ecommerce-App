0bd9f93573c10ad996a23e2181db5e74a36f0786
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-18 18:46:06 +0600

    test

--- src/App.js
+++ src/App.js
@@ -30,9 +30,7 @@ class App extends Component {
                 id: snapshot.id,
                 ...snapshot.data()
                 }
-              }, () => {
-                console.log(this.state)
-            })
+              })
            });
            
          } 
