051f5ba6b00a165b5453c1bd50c378c714d2d3dd
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-18 19:41:37 +0600

    sign component created

---
+++ src/components/sign-up/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FormInput from '../form-input/FormInput';
+import CustomButton from '../customButton/CustomButton';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+import './signUp.scss'
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <div>SignUp</div>
+    )
+  }
+}
