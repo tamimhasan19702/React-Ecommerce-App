4936072ac3710bc8c29737b9ef5845f395a5da66
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-20 20:50:56 +0600

    sign up layout setting up

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -5,9 +5,60 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './signUp.scss'
 
 export default class SignUp extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
   render() {
+    const {displayName, email, password, confirmPassword} = this.state;
     return (
-      <div>SignUp</div>
+      <div className='sign-up'>
+           <h2 className='title'>I do not have a account</h2>
+           <span>Sign up with your email and password</span>
+           <form className='sign-up-form' onSubmit={this.handleSubmit}>
+              
+              <FormInput 
+              type='text'
+              name='displayName'
+              value={displayName}
+              onChange={this.handleChange}
+              label='Display Name'
+              required />
+
+              <FormInput 
+              type='email'
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+              label='Email '
+              required />
+
+              <FormInput 
+              type='password'
+              name='password'
+              value={password}
+              onChange={this.handleChange}
+              label='Password'
+              required />
+               
+              <FormInput 
+              type='password'
+              name='confirmPassword'
+              value={confirmPassword}
+              onChange={this.handleChange}
+              label='Confirm Password'
+              required />
+              
+           </form>
+      </div>
     )
   }
 }
