c027ca347e1ff3c68125e460ce525a93800c1183
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-20 20:58:58 +0600

    finished adding the sign up component methods

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -17,8 +17,48 @@ export default class SignUp extends Component {
     }
   }
 
+  
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if(password !== confirmPassword) {
+      alert (`passoword don't match`);
+      return;
+    }
+
+    try{
+
+    const { user } = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+
+    await createUserProfileDocument(user, { displayName });
+
+    this.setState({
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    });
+
+    }catch(error){
+     console.error(error);
+    }
+  }
+
+  handleChange = event => {
+    const { name, value } =  event.target;
+
+    this.setState({[name]: value});
+  }
+
   render() {
+    
     const {displayName, email, password, confirmPassword} = this.state;
+
     return (
       <div className='sign-up'>
            <h2 className='title'>I do not have a account</h2>
@@ -56,6 +96,8 @@ export default class SignUp extends Component {
               onChange={this.handleChange}
               label='Confirm Password'
               required />
+
+              <CustomButton type="submit">SIGN UP</CustomButton>
               
            </form>
       </div>
