928f19e7ba1b536c744af23df1f3acebb429957d
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-24 00:43:36 +0600

    sign in completed with the new user

--- src/components/sign-in/Sign_in.jsx
+++ src/components/sign-in/Sign_in.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './sign_in.style.scss';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../customButton/CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
 
@@ -15,10 +15,18 @@ class SignIn extends Component {
   }
 }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ email: '', password: ''})
+    const { email, password } = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: ''})
+    }catch(error){
+      console.log(error);
+    }
+
   }
 
   handleChange = e => {
