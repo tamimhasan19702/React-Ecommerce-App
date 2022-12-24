14ca513f0cd3a3b9bbae12fd5661a13a8823c597
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-25 01:14:40 +0600

    redux added and learning userReducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+    
+    <Provider >
+      <BrowserRouter>
+      <App/>
+      </BrowserRouter>
+    </Provider>
   
 );
 
